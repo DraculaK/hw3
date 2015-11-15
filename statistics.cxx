7971eda9cb97ce1deb7f5fc766c20b813850eea0
Dimitri Kessler <dimitri@linux-mxoj.site>
2015-11-15 12:21:03 +0100

    fertig hw3

--- statistics.cxx
+++ statistics.cxx
@@ -1,19 +1,41 @@
 #include <cmath>
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 
 using namespace std;
-//--------------------------------------------------
-// plenty of lines here
-//--------------------------------------------------
+
+void ranum(double* p, const int N){
+  srand(time(NULL));
+  for (int i=0; i<N; i++){
+  double r = ((double) rand() / (RAND_MAX));
+  p[i] = r;
+  //cout << "p[" << i << "]=" << p[i] << endl;
+  }
+}
+
+void stat(double* p, const int N, double& mean, double& var){
+  double summean=0;
+  for(int i=0; i<N; i++){
+    summean += p[i];
+    mean = summean/N;
+  }
+  
+  double sumvar=0;
+  for(int i=0; i<N; i++){
+    sumvar += pow((p[i]-mean),2.0);
+    var = sumvar/N;
+  }
+}
 
 int main(){
    const int N = 100;
    double p[N];
    double mean, var;
 
-   // Some lines here....
-
+   ranum(p,N);
+   stat(p,N,mean,var);
+   
    cout << "Mean = " << mean << endl;
    cout << "Variance = " << var << endl;
 
